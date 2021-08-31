1e41fd8025c2ccb1cccb1638845332872ba4f6ea
yongjincho126 <yongjincho126@gmail.com>
2021-09-01 03:17:51 +0900

    update

--- new.js
+++ new.js
@@ -1,1 +1,15 @@
-location.href = "index.html";
+const SurveyInput = document.querySelector("#survey input");
+const SurveyButton = document.querySelector("#survey button");
+
+function handleSurveyBtnClick() {
+  console.log("submit!");
+}
+SurveyButton.addEventListener("click", handleSurveyBtnClick);
+
+const MiddleIframe = document.querySelector("#middle iframe");
+
+function handleMiddleBtnClick() {
+  console.log("click");
+}
+
+MiddleIframe.addEventListener("click", handleMiddleBtnClick);
