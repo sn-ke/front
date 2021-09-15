9a5aececf96dd933ff8b012199f7aab9056e67ba
yongjincho126 <yongjincho126@gmail.com>
2021-09-15 15:18:41 +0900

    update

--- new.js
+++ new.js
@@ -1,15 +1,88 @@
-const SurveyInput = document.querySelector("#survey input");
 const SurveyButton = document.querySelector("#survey button");
 
 function handleSurveyBtnClick() {
   console.log("submit!");
 }
-SurveyButton.addEventListener("click", handleSurveyBtnClick);
 
-const MiddleIframe = document.querySelector("#middle iframe");
+function doSave() {
+  let times = document.getElementsByName("times");
+  let checked_time_items = [];
+  for (let i = 0; i < times.length; i++) {
+    if (times[i].checked) {
+      checked_time_items.push(times[i].value);
+    }
+  }
 
-function handleMiddleBtnClick() {
-  console.log("click");
+  console.log(checked_time_items);
+}
+function doSave() {
+  let area = document.getElementsByName("area");
+  let checked_area_items = [];
+  for (let i = 0; i < area.length; i++) {
+    if (times[i].checked) {
+      checked_area_items.push(area[i].value);
+    }
+  }
+
+  console.log(checked_area_items);
+}
+function doSave() {
+  let purpose = document.getElementsByName("purpose");
+  let checked_purpose_items = [];
+  for (let i = 0; i < purpose.length; i++) {
+    if (purpose[i].checked) {
+      checked_purpose_items.push(purpose[i].value);
+    }
+  }
+
+  console.log(checked_purpose_items);
 }
 
-MiddleIframe.addEventListener("click", handleMiddleBtnClick);
+const firstVideoClick = document.getElementById("first");
+
+window.focus();
+
+window.addEventListener(
+  "blur",
+  () => {
+    setTimeout(() => {
+      if (document.activeElement.id === "first") {
+        firstVideoClick.textContent = "clicked " + Date.now();
+        console.log("firstvideoclick");
+      }
+    });
+  },
+  { once: true }
+);
+const secondVideoClick = document.getElementById("second");
+
+window.focus();
+
+window.addEventListener(
+  "blur",
+  () => {
+    setTimeout(() => {
+      if (document.activeElement.id === "second") {
+        secondVideoClick.textContent = "clicked " + Date.now();
+        console.log("secondvideoclick");
+      }
+    });
+  },
+  { once: true }
+);
+const thirdVideoClick = document.getElementById("third");
+
+window.focus();
+
+window.addEventListener(
+  "blur",
+  () => {
+    setTimeout(() => {
+      if (document.activeElement.id === "third") {
+        thirdVideoClick.textContent = "clicked " + Date.now();
+        console.log("thirdvideoclick");
+      }
+    });
+  },
+  { once: true }
+);
